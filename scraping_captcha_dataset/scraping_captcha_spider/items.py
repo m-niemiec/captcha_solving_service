5f95badf39458268926bc0d112edf93a655996da
mniemiec <mniemiec@appriss.com>
2021-08-03 11:33:01 +0200

    Deleted redundant code

--- scraping_captcha_dataset/scraping_captcha_spider/items.py
+++ scraping_captcha_dataset/scraping_captcha_spider/items.py
@@ -5,8 +5,3 @@ class CaptchaImageItem(Item):
     image_urls = Field()
     images = Field()
     image_name = Field()
-
-
-class CaptchaImageFileItem(Item):
-    file_urls = Field()
-    files = Field

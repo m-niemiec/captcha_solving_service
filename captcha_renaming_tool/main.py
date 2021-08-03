3a596d9040f46d731e1da98db4099c2bf362fb6e
mniemiec <mniemiec@appriss.com>
2021-08-03 18:45:50 +0200

    Displaying captcha image, basic labels

--- captcha_renaming_tool/main.py
+++ captcha_renaming_tool/main.py
@@ -1,4 +1,5 @@
 import os
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk, filedialog
 
@@ -10,7 +11,8 @@ root = Tk()
 root.title("Captcha Renaming Tool")
 
 # Size of the screen
-root.geometry('700x350')
+screen_size = (700, 350)
+root.geometry(f'{screen_size[0]}x{screen_size[1]}')
 
 image_list = []
 
@@ -18,6 +20,10 @@ image_list = []
 def get_folder_path():
     folder_selected = filedialog.askdirectory()
     folder_path.set(folder_selected)
+
+
+def rename_image_file(event):
+    print('rename_image_file')
 
 
 def do_stuff():
@@ -29,12 +35,28 @@ def do_stuff():
             tk_image = ImageTk.PhotoImage(image)
             image_list.append(tk_image)
 
+    label = Label(image=image_list[0])
+    label.grid(row=1, column=0, columnspan=3)
+    label.place(relx=0.5, rely=0.5, anchor='center')
+
+    captcha_solution = tk.Entry(root, textvariable=captcha_solution_text)
+    captcha_solution.insert(0, '(type captcha solution here)')
+    captcha_solution.grid(row=0, column=0)
+    captcha_solution.place(relx=0.5, rely=0.8, anchor='center')
+
+    captcha_solution.bind('<Return>', rename_image_file)
+    # captcha_solution.pack()
+    # res = tk.Label(root)
+    # res.pack()
+
 
 folder_path = StringVar()
-button_find_captchas = ttk.Button(root, text='Browse Folder', command=get_folder_path)
-button_find_captchas.grid(row=0, column=2)
-c = ttk.Button(root, text='Find Captchas', command=do_stuff)
-c.grid(row=4, column=0)
+captcha_solution_text = StringVar()
+
+button_select_folder = ttk.Button(root, text='Select Folder', command=get_folder_path)
+button_select_folder.grid(row=0, column=0)
+c = ttk.Button(root, text='Load Captchas', command=do_stuff)
+c.grid(row=0, column=2)
 
 button_exit = Button(root, text="Exit", command=root.quit)
 

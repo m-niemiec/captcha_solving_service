d3cd97490630196974cca2e96e24e6803dff714d
mniemiec <mniemiec@appriss.com>
2021-08-04 17:44:18 +0200

    Code refactor to use classes

--- captcha_renaming_tool/main.py
+++ captcha_renaming_tool/main.py
@@ -1,65 +1,71 @@
 import os
-import re
 import tkinter as tk
+import re
 from tkinter import *
 from tkinter import ttk, filedialog
 
 from PIL import ImageTk, Image
 
-root = Tk()
 
-# Title of app
-root.title("Captcha Renaming Tool")
+class CaptchaRenamingTool:
+    folder_path = None
+    captcha_solution_text = None
+    root = None
+    image_list = []
+    
+    def __init__(self):
+        self.root = Tk()
 
-# Size of the screen
-screen_size = (700, 350)
-root.geometry(f'{screen_size[0]}x{screen_size[1]}')
+        # Title of app
+        self.root.title("Captcha Renaming Tool")
 
-image_list = []
+        # Size of the screen
+        screen_size = (700, 350)
+        self.root.geometry(f'{screen_size[0]}x{screen_size[1]}')
+
+        self.folder_path = StringVar()
+        self.captcha_solution_text = StringVar()
+
+        button_select_folder = ttk.Button(self.root, text='Select Folder', command=self.get_folder_path)
+        button_select_folder.grid(row=0, column=0)
+        c = ttk.Button(self.root, text='Load Captchas', command=self.import_images)
+        c.grid(row=0, column=2)
+
+        self.root.mainloop()
+
+    def get_folder_path(self):
+        folder_selected = filedialog.askdirectory()
+        self.folder_path.set(folder_selected)
+
+    def rename_image_file(self, event):
+        image_path = self.image_list[0][1].filename
+        image_directory = re.findall(r'(.+\/)', image_path)[0]
+        image_extension = re.findall(r'(\.\w+)', image_path)[0]
+
+        os.rename(image_path, image_directory + self.captcha_solution_text.get() + image_extension)
+
+    def import_images(self):
+        folder = self.folder_path.get()
+
+        for image in os.listdir(folder):
+            if image.endswith('jpeg'):
+                image = Image.open(f'{folder}/{image}')
+                tk_image = ImageTk.PhotoImage(image)
+                self.image_list.append((tk_image, image))
+
+        label = Label(image=self.image_list[0][0])
+        label.grid(row=1, column=0, columnspan=3)
+        label.place(relx=0.5, rely=0.5, anchor='center')
+
+        captcha_solution = tk.Entry(self.root, textvariable=self.captcha_solution_text)
+        captcha_solution.insert(0, '(type captcha solution here)')
+        captcha_solution.grid(row=0, column=0)
+        captcha_solution.place(relx=0.5, rely=0.8, anchor='center')
+
+        captcha_solution.bind('<Return>', self.rename_image_file)
+
+    # button_exit = Button(root, text="Exit", command=root.quit)
 
 
-def get_folder_path():
-    folder_selected = filedialog.askdirectory()
-    folder_path.set(folder_selected)
-
-
-def rename_image_file(event):
-    image_path = image_list[0][1].filename
-    image_directory = re.findall(r'(.+\/)', image_path)[0]
-    image_extension = re.findall(r'(\.\w+)', image_path)[0]
-
-    os.rename(image_path, image_directory + captcha_solution_text.get() + image_extension)
-
-
-def do_stuff():
-    folder = folder_path.get()
-
-    for image in os.listdir(folder):
-        if image.endswith('jpeg'):
-            image = Image.open(f'{folder}/{image}')
-            tk_image = ImageTk.PhotoImage(image)
-            image_list.append((tk_image, image))
-
-    label = Label(image=image_list[0][0])
-    label.grid(row=1, column=0, columnspan=3)
-    label.place(relx=0.5, rely=0.5, anchor='center')
-
-    captcha_solution = tk.Entry(root, textvariable=captcha_solution_text)
-    captcha_solution.insert(0, '(type captcha solution here)')
-    captcha_solution.grid(row=0, column=0)
-    captcha_solution.place(relx=0.5, rely=0.8, anchor='center')
-
-    captcha_solution.bind('<Return>', rename_image_file)
-
-
-folder_path = StringVar()
-captcha_solution_text = StringVar()
-
-button_select_folder = ttk.Button(root, text='Select Folder', command=get_folder_path)
-button_select_folder.grid(row=0, column=0)
-c = ttk.Button(root, text='Load Captchas', command=do_stuff)
-c.grid(row=0, column=2)
-
-button_exit = Button(root, text="Exit", command=root.quit)
-
-root.mainloop()
+if __name__ == '__main__':
+    CaptchaRenamingTool()

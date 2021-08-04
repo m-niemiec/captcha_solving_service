c6fccde3e677e67beb8fd37754ea6338516f094a
mniemiec <mniemiec@appriss.com>
2021-08-04 18:47:38 +0200

    Label with captcha name, renaming improvments

--- captcha_renaming_tool/main.py
+++ captcha_renaming_tool/main.py
@@ -3,15 +3,19 @@ import tkinter as tk
 import re
 from functools import partial
 from tkinter import *
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 
 from PIL import ImageTk, Image
+
+from helper_texts import show_help_text
 
 
 class CaptchaRenamingTool:
     folder_path = None
     captcha_solution_text = None
     root = None
+    current_captcha_name = None
+    current_captcha_image = None
     image_index = 0
     image_list = []
     
@@ -27,11 +31,16 @@ class CaptchaRenamingTool:
 
         self.folder_path = StringVar()
         self.captcha_solution_text = StringVar()
+        self.current_captcha_name = StringVar()
 
-        button_select_folder = ttk.Button(self.root, text='Select Folder', command=self.get_folder_path)
-        button_select_folder.grid(row=0, column=0)
-        c = ttk.Button(self.root, text='Load Captchas', command=self.import_images)
-        c.grid(row=0, column=2)
+        select_folder_button = ttk.Button(self.root, text='Select Folder', command=self.get_folder_path)
+        select_folder_button.grid(row=0, column=0)
+
+        import_captchas_button = ttk.Button(self.root, text='Load Captchas', command=self.import_captchas)
+        import_captchas_button.grid(row=0, column=2)
+
+        show_help_button = ttk.Button(self.root, text='Show Help', command=self.show_help)
+        show_help_button.place(relx=0.5, rely=0.5, anchor='center')
 
         self.root.mainloop()
 
@@ -39,14 +48,7 @@ class CaptchaRenamingTool:
         folder_selected = filedialog.askdirectory()
         self.folder_path.set(folder_selected)
 
-    def rename_image_file(self, event):
-        image_path = self.image_list[self.image_index][1].filename
-        image_directory = re.findall(r'(.+\/)', image_path)[0]
-        image_extension = re.findall(r'(\.\w+)', image_path)[0]
-
-        os.rename(image_path, image_directory + self.captcha_solution_text.get() + image_extension)
-
-    def import_images(self):
+    def import_captchas(self):
         folder = self.folder_path.get()
 
         for image in os.listdir(folder):
@@ -55,38 +57,66 @@ class CaptchaRenamingTool:
                 tk_image = ImageTk.PhotoImage(image)
                 self.image_list.append((tk_image, image))
 
-        current_captcha_image = Label(image=self.image_list[self.image_index][0])
-        current_captcha_image.grid(row=1, column=0, columnspan=3)
-        current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
+        self.current_captcha_image = Label(image=self.image_list[self.image_index][0])
+        self.current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
+
+        self.update_image_label()
 
         captcha_solution = tk.Entry(self.root, textvariable=self.captcha_solution_text)
         captcha_solution.insert(0, '(type captcha solution here)')
-        captcha_solution.grid(row=0, column=0)
         captcha_solution.place(relx=0.5, rely=0.8, anchor='center')
 
         captcha_solution.bind('<Return>', self.rename_image_file)
 
         next_image_button = ttk.Button(self.root, text='Next',
-                                       command=partial(self.switch_captcha_image, current_captcha_image, 1))
-        next_image_button.grid(row=0, column=0)
+                                       command=partial(self.switch_captcha_image, 1))
         next_image_button.place(relx=0.85, rely=0.5, anchor='center')
 
         previous_image_button = ttk.Button(self.root, text='Previous',
-                                           command=partial(self.switch_captcha_image, current_captcha_image, -1))
-        previous_image_button.grid(row=0, column=0)
+                                           command=partial(self.switch_captcha_image, -1))
         previous_image_button.place(relx=0.15, rely=0.5, anchor='center')
 
-    def switch_captcha_image(self, current_captcha_image, direction):
-        current_captcha_image.destroy()
+    def rename_image_file(self, event):
+        old_image_path = self.image_list[self.image_index][1].filename
+        image_directory = re.findall(r'(.+\/)', old_image_path)[0]
+        image_extension = re.findall(r'(\.\w+)', old_image_path)[0]
 
+        new_image_path = image_directory + self.captcha_solution_text.get() + image_extension
+
+        os.rename(old_image_path, new_image_path)
+
+        self.image_list[self.image_index][1].filename = new_image_path
+
+        self.switch_captcha_image()
+
+    def switch_captcha_image(self, direction=1):
         try:
-            current_captcha_image = Label(image=self.image_list[self.image_index+direction][0])
-            current_captcha_image.grid(row=1, column=0, columnspan=3)
-            current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
+            image = self.image_list[self.image_index+direction][0]
+            self.current_captcha_image.destroy()
+            self.current_captcha_image = Label(image=image)
+            self.current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
 
             self.image_index += direction
+
+            self.update_image_label()
         except IndexError:
             pass
+
+    def update_image_label(self):
+        image_path = self.image_list[self.image_index][1].filename
+        captcha_name = re.findall(r'.+\/(.+)\.', image_path)[0]
+
+        try:
+            self.current_captcha_name.destroy()
+        except AttributeError:
+            pass
+
+        self.current_captcha_name = Label(text=captcha_name)
+        self.current_captcha_name.place(relx=0.5, rely=0.25, anchor='center')
+
+    @staticmethod
+    def show_help():
+        messagebox.showinfo('Help', show_help_text)
 
 
 if __name__ == '__main__':

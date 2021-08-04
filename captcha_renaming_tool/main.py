0b2ebef6775018ba613a4f2bb137ac3ae7509d90
mniemiec <mniemiec@appriss.com>
2021-08-04 18:04:08 +0200

    Next and previous captcha buttons

--- captcha_renaming_tool/main.py
+++ captcha_renaming_tool/main.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 import re
+from functools import partial
 from tkinter import *
 from tkinter import ttk, filedialog
 
@@ -11,6 +12,7 @@ class CaptchaRenamingTool:
     folder_path = None
     captcha_solution_text = None
     root = None
+    image_index = 0
     image_list = []
     
     def __init__(self):
@@ -38,7 +40,7 @@ class CaptchaRenamingTool:
         self.folder_path.set(folder_selected)
 
     def rename_image_file(self, event):
-        image_path = self.image_list[0][1].filename
+        image_path = self.image_list[self.image_index][1].filename
         image_directory = re.findall(r'(.+\/)', image_path)[0]
         image_extension = re.findall(r'(\.\w+)', image_path)[0]
 
@@ -53,9 +55,9 @@ class CaptchaRenamingTool:
                 tk_image = ImageTk.PhotoImage(image)
                 self.image_list.append((tk_image, image))
 
-        label = Label(image=self.image_list[0][0])
-        label.grid(row=1, column=0, columnspan=3)
-        label.place(relx=0.5, rely=0.5, anchor='center')
+        current_captcha_image = Label(image=self.image_list[self.image_index][0])
+        current_captcha_image.grid(row=1, column=0, columnspan=3)
+        current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
 
         captcha_solution = tk.Entry(self.root, textvariable=self.captcha_solution_text)
         captcha_solution.insert(0, '(type captcha solution here)')
@@ -64,7 +66,27 @@ class CaptchaRenamingTool:
 
         captcha_solution.bind('<Return>', self.rename_image_file)
 
-    # button_exit = Button(root, text="Exit", command=root.quit)
+        next_image_button = ttk.Button(self.root, text='Next',
+                                       command=partial(self.switch_captcha_image, current_captcha_image, 1))
+        next_image_button.grid(row=0, column=0)
+        next_image_button.place(relx=0.85, rely=0.5, anchor='center')
+
+        previous_image_button = ttk.Button(self.root, text='Previous',
+                                           command=partial(self.switch_captcha_image, current_captcha_image, -1))
+        previous_image_button.grid(row=0, column=0)
+        previous_image_button.place(relx=0.15, rely=0.5, anchor='center')
+
+    def switch_captcha_image(self, current_captcha_image, direction):
+        current_captcha_image.destroy()
+
+        try:
+            current_captcha_image = Label(image=self.image_list[self.image_index+direction][0])
+            current_captcha_image.grid(row=1, column=0, columnspan=3)
+            current_captcha_image.place(relx=0.5, rely=0.5, anchor='center')
+
+            self.image_index += direction
+        except IndexError:
+            pass
 
 
 if __name__ == '__main__':

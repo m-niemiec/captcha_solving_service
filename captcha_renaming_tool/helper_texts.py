34bd9dcdc9ea01f56e52ba5981477995087a31b8
mniemiec <mniemiec@appriss.com>
2021-08-16 17:32:53 +0200

    Updated help text

--- captcha_renaming_tool/helper_texts.py
+++ captcha_renaming_tool/helper_texts.py
@@ -6,7 +6,8 @@ On top you will see buttons "Zoom IN", "Zoom OUT" and "B&W Mode".\n
 Those buttons will make your images larger, smaller and convert them to black and white (and backward) respectively.\n\n
 
 After that, you can just start to type new names for your files and press ENTER.\n
-ENTER will save the new name and automatically go to the next image.\n\n
+ENTER will save the new name and automatically go to the next image.\n
+Arrows UP and DOWN will show next and previous images.\n\n
 
 Created by Michał Niemiec.\n
 www.michalniemiec.info

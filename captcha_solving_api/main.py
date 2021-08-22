81812bb82aa3ae24baf63a9b1ed8ce850cbe29a4
mniemiec <mniemiec@appriss.com>
2021-08-22 15:04:59 +0200

    Added health check

--- captcha_solving_api/main.py
+++ captcha_solving_api/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+@app.get('/health_check')
+async def health_check():
+    return {'message': 'I am alive and kicking!'}

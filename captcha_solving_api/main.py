bde4f49e80237d582884e2d55792368e5b137d3f
mniemiec <mniemiec@appriss.com>
2021-08-27 18:16:54 +0200

    Added usage of all new clases for solving captchas

--- captcha_solving_api/main.py
+++ captcha_solving_api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from logzero import logger
 
+from evaluate_request import EvaluateRequest
+from recognize_captcha_type import RecognizeCaptchaType
 from solve_captcha import SolveCaptcha
 
 app = FastAPI()
@@ -8,7 +10,10 @@ app = FastAPI()
 
 @app.post("/upload_captcha")
 async def create_upload_file(captcha_image: UploadFile = File(...), api_key: str = Form(...)):
-    solution = await SolveCaptcha().get_solution(captcha_image)
+    image_format, image_path = await EvaluateRequest().analyze_image(captcha_image)
+    captcha_type = await RecognizeCaptchaType().get_captcha_type(image_path)
+
+    solution = await SolveCaptcha().get_solution(image_format, image_path, captcha_type)
 
     return solution
 

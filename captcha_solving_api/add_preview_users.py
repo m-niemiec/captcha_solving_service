4adb10323c705ae6d045c2ab40b551f05c22fcdc
mniemiec <mniemiec@appriss.com>
2021-08-31 14:50:11 +0200

    Added additional 'admin' user

--- captcha_solving_api/add_preview_users.py
+++ captcha_solving_api/add_preview_users.py
@@ -4,7 +4,18 @@ from manage_authorizations import ManageAuthorization
 from models import User as ModelUser
 
 
-def add_preview_user():
+def add_preview_users():
+    # Add admin user with user id - 1
+    hashed_password = ManageAuthorization().get_password_hash('admin')
+
+    user_db = ModelUser(username='admin',
+                        email='admin@admin.com',
+                        credit_balance=999999,
+                        password=hashed_password)
+    db.session.add(user_db)
+    db.session.commit()
+
+    # Add test user
     hashed_password = ManageAuthorization().get_password_hash('test')
 
     user_db = ModelUser(username='test',

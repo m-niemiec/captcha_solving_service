873d4bea4c54d22cbf69070e59dfad635620583c
mniemiec <mniemiec@appriss.com>
2021-08-31 14:59:37 +0200

    Deleted redundant code

--- captcha_solving_api/manage_authorizations.py
+++ captcha_solving_api/manage_authorizations.py
@@ -19,17 +19,6 @@ class TokenData(BaseModel):
     username: Optional[str] = None
 
 
-# class User(BaseModel):
-#     username: str
-#     email: Optional[str] = None
-#     full_name: Optional[str] = None
-#     disabled: Optional[bool] = None
-#
-#
-# class UserInDB(User):
-#     hashed_password: str
-
-
 class ManageAuthorization:
     SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
     ALGORITHM = 'HS256'

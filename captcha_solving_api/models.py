1f73e49d7bdff6e9810901cf5d9a4da51328b172
mniemiec <mniemiec@appriss.com>
2021-09-27 19:13:15 +0200

    Fixed 'on_delete' errors

--- captcha_solving_api/models.py
+++ captcha_solving_api/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 Base = declarative_base()
@@ -11,9 +11,9 @@ class CreditsTransaction(Base):
     id = Column(Integer, primary_key=True, index=True)
     credit_amount = Column(Integer)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
 
-    user = relationship('User', backref=backref("CreditsTransaction", cascade="all,delete"))
+    user = relationship('User', back_populates='credits_transaction')
 
 
 class CaptchaSolveQuery(Base):
@@ -23,9 +23,9 @@ class CaptchaSolveQuery(Base):
     captcha_type = Column(String)
     captcha_solution = Column(String)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
 
-    user = relationship('User', backref=backref("CaptchaSolveQuery", cascade="all,delete"))
+    user = relationship('User', back_populates='captcha_solve_query')
 
 
 class User(Base):
@@ -38,5 +38,11 @@ class User(Base):
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
-    # credits_transaction = relationship("CreditsTransaction", cascade="all,delete", backref="parent")
-    # captcha_solve_query = relationship("CaptchaSolveQuery", cascade="all,delete", backref="parent")
+    credits_transaction = relationship('CreditsTransaction',
+                                       back_populates='user',
+                                       cascade='all,delete',
+                                       passive_deletes=True)
+    captcha_solve_query = relationship('CaptchaSolveQuery',
+                                       back_populates='user',
+                                       cascade='all,delete',
+                                       passive_deletes=True)
